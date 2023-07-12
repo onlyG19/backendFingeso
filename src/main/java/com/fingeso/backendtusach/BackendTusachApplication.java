1102e9f4a1a78780dc44277de2005247287410c0
SpiderKnightAcademico <john.fernandez.c@usach.cl>
2023-07-12 12:57:10 -0400

    [John Fernandez]: Agregado CORS a todo el proyecto

--- src/main/java/com/fingeso/backendtusach/BackendTusachApplication.java
+++ src/main/java/com/fingeso/backendtusach/BackendTusachApplication.java
@@ -2,9 +2,12 @@ package com.fingeso.backendtusach;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
@@ -29,7 +32,13 @@ public class BackendTusachApplication {
 		);
 	}
 
-
-
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 }
